12e40ecebb293231f5271060f3c73f9000eff57a
nnigorenko <167545999+nnigorenko@users.noreply.github.com>
2024-04-29 19:10:40 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-# print('Привет!')
-# My_number = int( input('Введите целое число: '))
-# if My_number < 0:
-#     print('Отрицательное')
-# elif My_number > 0:
-#     print('Положительное')
-# else:
-#     print('Ноль')
-# print('Пока!')
-# Примеры
-# a, b, c = 15, 5, -10
-# if not c > 0 and b - a == c:
-#     print('Success!')
-# if a > b or (c < b and a > c):
-#     print('Success!')
-# if a - b + c == 0:
-#     print('Success!')
-# if b ** 2 + c ** 2 - a >= 100:
-#     print('Success!')
+print('Привет!')
+My_number = int( input('Введите целое число: '))
+if My_number < 0:
+    print('Отрицательное')
+elif My_number > 0:
+    print('Положительное')
+else:
+    print('Ноль')
+print('Пока!')
+Примеры
+a, b, c = 15, 5, -10
+if not c > 0 and b - a == c:
+    print('Success!')
+if a > b or (c < b and a > c):
+    print('Success!')
+if a - b + c == 0:
+    print('Success!')
+if b ** 2 + c ** 2 - a >= 100:
+    print('Success!')
 if '56' > '234':
     print('Success!')
 if {'Cat' : 'Кот'} != {'Dog' : 'Пёс'}:
