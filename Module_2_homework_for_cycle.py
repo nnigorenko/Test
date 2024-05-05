ba15b64725e5fe9853fd4daf1ef6307b5565a4c4
nnigorenko <167545999+nnigorenko@users.noreply.github.com>
2024-05-05 10:02:29 +0300

    Add files via upload

---
+++ Module_2_homework_for_cycle.py
@@ -0,0 +1,8 @@
+car_list = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in range(0, len(car_list)):
+    cars_count += 10
+ #  print('I use my', car_list[i], 'car', cars_count)
+ # Впрочем, задание не требует выводить значение cars_count
+ # поэтому последнюю строку запишем так:
+    print('I use my', car_list[i], 'car')
