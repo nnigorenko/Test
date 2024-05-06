9f84aedb1acc747c4ba97a5962353d8374930199
nnigorenko <167545999+nnigorenko@users.noreply.github.com>
2024-05-06 22:44:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,8 @@
-print('Привет!')
-My_number = int( input('Введите целое число: '))
-if My_number < 0:
-    print('Отрицательное')
-elif My_number > 0:
-    print('Положительное')
-else:
-    print('Ноль')
-print('Пока!')
-Примеры
-a, b, c = 15, 5, -10
-if not c > 0 and b - a == c:
-    print('Success!')
-if a > b or (c < b and a > c):
-    print('Success!')
-if a - b + c == 0:
-    print('Success!')
-if b ** 2 + c ** 2 - a >= 100:
-    print('Success!')
-if '56' > '234':
-    print('Success!')
-if {'Cat' : 'Кот'} != {'Dog' : 'Пёс'}:
-    print('Success!')
-if [-1, 0, 1] <= [1, 2, 3]:
-    print('Success!')
-if (5, 25) < (25, 5):
-    print('Success!')
-# if '6' > 5:
-#     print('Success!')
-# if 9 > [1, 2]:
-#     print('Success!')
-if {1 : "first"} != (1 , 'first'):
-    print('Success!!')
+def test():
+    a = True
+    b = 125
+    print(a, b)
+test()
+def test2(c, b, a):
+    print(a, b, c)
+test2(3, 2, 1)
